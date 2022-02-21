c900d50acbfcd3638e074ff9ec2dbf0540bde84c
Dustin-Har <harbaughdustin@yahoo.com>
2022-02-21 13:02:26 -0500

    Add scss

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Game() {
 	return (
-		<div>
-			
+		<div class="taco-tapper">
+			<div class="tacos">
+				<h2>1,243 Tacos</h2>
+				<h3>3 Tacos per second</h3>
+			</div>
+			<button class="taco-button">
+				<img src="../assets/Taco.png" alt="pixel art of a taco" class="taco-img"/>
+			</button>
 		</div>
 	)
 }
