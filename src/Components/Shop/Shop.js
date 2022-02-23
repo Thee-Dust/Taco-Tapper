07c63cd4e6139210d74a034f3d09a65086c75bec
Dustin-Har <harbaughdustin@yahoo.com>
2022-02-22 23:23:48 -0500

    Add shop Option component

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,26 +1,29 @@
 import React from 'react'
-import taco from '../../Assets/Taco.png'
+import ShopOptionsData from '../../ShopData/ShopData.json'
 import './Shop.scss'
+import ShopOption from '../ShopOption/ShopOption'
 
 export default function Shop({ totalTacos, tacosSpent }) {
-	
 
+const shopOptionCards = ShopOptionsData.map(option => {
+	return(
+		<ShopOption 
+		totalTacos={totalTacos}
+		tacosSpent={tacosSpent}
+		name={option.name}
+		level={option.level}
+		price={option.price}
+		description={option.description}
+		/>
+	)
+})
 
 
 	return (
 		<div className="shop">
 			<h2>Shop</h2>
 			<div className="shop-upgrades">
-				<button className="upgrade">
-					<div className="top-upgrade-row">
-						<p>Taco Tuesday</p>
-						<p>LVL: 1</p>
-					</div>
-					<div className="bottom-upgrade-row">
-						<p><img src={taco} alt="pixel art of a taco" className="taco-image"/>100</p>
-						<p>adds 1 Taco per click</p>
-					</div>
-				</button>
+				{shopOptionCards}
 			</div>
 		</div>
 	)
---
+++ src/Components/ShopOption/ShopOption.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import taco from '../../Assets/Taco.png'
+
+export default function ShopOption({ totalTacos, tacosSpent, name, level, price, description }) {
+	//checks to see if user has enough tacos to spend on upgrade
+	const isDisabled = totalTacos > price ? true : false;
+
+	const spendTacos = (num) => {
+		tacosSpent(num)
+	}
+	
+	return (
+		<button className="upgrade" onClick={() => spendTacos(price)} disabled={isDisabled}>
+			<div className="top-upgrade-row">
+				<p>{name}</p>
+				<p>LVL: {level}</p>
+			</div>
+			<div className="bottom-upgrade-row">
+				<p><img src={taco} alt="pixel art of a taco" className="taco-image"/>{price}</p>
+				<p>{description}</p>
+			</div>
+		</button>
+	)
+}
